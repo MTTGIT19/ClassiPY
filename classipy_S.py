6383b8f61d391929db997ec8b4aa3d8659d81f83
Matt <89365060+MTTGIT19@users.noreply.github.com>
2024-01-09 00:31:32 -0600

    Update classipy_S.py

--- classipy_S.py
+++ classipy_S.py
@@ -6,26 +6,32 @@ from wand.drawing import Drawing
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 input_folder = os.path.join(current_directory, 'input_images')  # Folder named input_images
-output_folder = ('/home/'+ os.getlogin() +'/Desktop/SECRET_IMAGES/')  # Folder named output_images
+output_folder = ('/home/'+ os.getlogin() +'/Desktop/CUI_IMAGES/')  # Folder named output_images
 
-# If you'd like to get fancy you can edit the next two lines to change the input/output paths. 
-#input_folder = '/home/' + os.getlogin() + '/Downloads/screenshots1/' ## CHANGE ME TO SCREENSHOT PATH
-#output_folder = '/home/' + os.getlogin() + '/Desktop/SECRET_IMAGES/' # Output location: default desktop
+banner_percentage_of_height = 5  # Adjust the percentage as needed
+font_percentage_of_banner = 50  # Adjust the font size percentage as needed
 
 def add_banner_and_border_to_image(input_image, output_image):
     with Image(filename=input_image) as img:
-        banner_height = int(img.height * 0.05)
+        banner_height = int(img.height * (banner_percentage_of_height / 100))
         banner = Image(width=img.width, height=banner_height)
         banner.background_color = Color('red')
         banner.alpha_channel = 'remove'
+        
+        # Calculate font size based on a percentage of the banner's height
+        font_size = int(banner_height * (font_percentage_of_banner / 100))
 
         with Drawing() as draw:
             draw.font = '/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf'  # Path to your font file or try 'Times new roman'
-            draw.font_size = 14
+            draw.font_size = font_size
             draw.fill_color = Color('white')
             draw.text_alignment = 'center'
-            draw.text(int(banner.width / 2), int(banner.height / 2) + 3, 'SECRET')
+            draw.text(x=banner.width // 2, y=banner_height // 2 + font_size // 3, body='SECRET')
+            draw.text(x=banner.width // 2, y=img.height + banner_height + banner_height // 2 + font_size // 3, body='SECRET')
             draw(banner)
+
+            # Calculate font size based on the banner's height
+           # font_size = int(banner_height * 6)
 
         with Image(filename=input_image) as img:  # Re-open the image to avoid closure issue
             with Image(width=img.width, height=img.height + 2 * banner_height) as result:
@@ -41,7 +47,7 @@ def label_folder(input_folder, output_folder):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    for root, dirs, files in os.walk(input_folder):
+    for root, dirs, files in os.walk(current_directory):
         for filename in files:
             if filename.lower().endswith(".jpg") or filename.lower().endswith(".png"):
                 input_path = os.path.join(root, filename)
