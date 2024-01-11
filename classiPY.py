7a16cfa55bab73d1e15631728a08b0008612f0ab
slab14 <mmccorm1@andrew.cmu.edu>
2024-01-10 18:15:35 -0600

    refactoring to clean up logic

--- classiPY.py
+++ classiPY.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-import pathlib
+from pathlib import Path
 from wand.image import Image
 from wand.color import Color
 from wand.drawing import Drawing
@@ -10,14 +10,32 @@ banner_percent_of_height = 5  # Adjust the percentage as needed
 font_percent_of_banner = 50  # Adjust the font size percentage as needed
 extensions = ['.jpg', '.png', '.jpeg'] # file extensions to look for
 
+class Markings:
+    def __init__(self, classifications):
+        self.classifications=classifications
+    def get_symbol(self, classification):
+        if classification=='CUI':
+            return 'CUI'
+        elif classification=='S':
+            return 'S'
+    def get_long_name(self, classification):
+        if classification=='CUI':
+            return 'CUI'
+        elif classification=='S':
+            return 'SECRET'
+    def get_color(self, classification):
+        if classification=='CUI':
+            return '#502b85'
+        elif classification=='S':
+            return 'red'
+
+markings = Markings(['CUI', 'S'])
+        
 def add_banner(input_image, output_image, classification='CUI'):
     with Image(filename=input_image) as img:
         banner_height = int(img.height * (banner_percent_of_height / 100))
         banner = Image(width=img.width, height=banner_height)
-        if classification=='CUI':
-            banner.background_color = Color('#502b85')
-        elif classification=='S':
-            banner.background_color = Color('red')
+        banner.background_color = markings.get_color(classification)
         banner.alpha_channel = 'remove'
         
         # Calculate font size based on a percentage of the banner's height
@@ -28,18 +46,13 @@ def add_banner(input_image, output_image, classification='CUI'):
             draw.font_weight = 700  # Bold font weight
             draw.fill_color = Color('white')
             draw.text_alignment = 'center'
-            if classification=='CUI':
-                draw.text(x=banner.width // 2,
-                          y=banner_height // 2 + font_size // 3, body='CUI')
-                draw.text(x=banner.width // 2,
-                          y=img.height + banner_height +
-                          banner_height // 2 + font_size // 3, body='CUI')
-            elif classification=='S':
-                draw.text(x=banner.width // 2,
-                          y=banner_height // 2 + font_size // 3, body='SECRET')
-                draw.text(x=banner.width // 2,
-                          y=img.height + banner_height +
-                          banner_height // 2 + font_size // 3, body='SECRET')                
+            draw.text(x=banner.width // 2,
+                      y=banner_height // 2 + font_size // 3,
+                      body=markings.get_long_name(classification))
+            draw.text(x=banner.width // 2,
+                      y=img.height + banner_height +
+                      banner_height // 2 + font_size // 3,
+                      body=markings.get_long_name(classification))
             draw(banner)
 
         with Image(filename=input_image) as img:  # Re-open the image to avoid closure issue
@@ -55,19 +68,16 @@ def add_banner(input_image, output_image, classification='CUI'):
                 result.save(filename=output_image)
 
 def label_folder(input_folder, output_folder, classification='CUI'):
-    if not pathlib.Path(output_folder).exists():
+    if not Path(output_folder).exists():
         output_folder.mkdir(parents=True, exist_ok=True)
 
-    image_files = [x for x in pathlib.Path(input_folder).iterdir()
+    image_files = [x for x in Path(input_folder).iterdir()
                    if x.suffix.lower() in extensions]
     
     for image_file in image_files:
-        if classification=='CUI':
-            output_file = pathlib.Path(output_folder,
-                                       f"(CUI) {image_file.name}")
-        elif classification=='S':
-            output_file = pathlib.Path(output_folder,
-                                       f"(S) {image_file.name}")
+        output_file = Path(output_folder,
+                           f"({markings.get_symbol(classification)}) \
+                           {image_file.name}")
         add_banner(image_file, output_file, classification)
 
 def main():
@@ -76,7 +86,7 @@ def main():
     # Adding required arguments as group
     required_group = parser.add_argument_group('Required arguments')
     required_group.add_argument('--image', '-I', required=True, type=str, help='Folder path to orginal images')
-    required_group.add_argument('--classification', '-C', required=True, type=str, choices=['CUI', 'S'],
+    required_group.add_argument('--classification', '-C', required=True, type=str, choices=markings.classifications,
                                 help='Classification level (S or CUI)')
 
     # Adding optional argument as group
@@ -86,8 +96,8 @@ def main():
     args = parser.parse_args()
 
     # Resolving and setting absolute paths using pathlib
-    args.image = pathlib.Path(args.image).resolve()
-    args.output = args.image if args.output is None else pathlib.Path(args.output).resolve()
+    args.image = Path(args.image).resolve()
+    args.output = args.image if args.output is None else Path(args.output).resolve()
 
     label_folder(args.image, args.output, args.classification)
 
